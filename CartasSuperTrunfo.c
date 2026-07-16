32959e7ed06edc9cbbf5377ee5dfc7d6e1864f3a
Valtecio Silva Almeida <valteciosalmeida@gmail.com>
2026-07-16 10:38:32 +0000

    Entrega Desafio: nível mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-#include <windows.h>
+// #include <windows.h> // Resolução de caracteres especiais no terminal do windows
 
 int main(void) {
-    SetConsoleOutputCP(CP_UTF8); // Resolução de caracteres especiais no terminal
-    SetConsoleCP(CP_UTF8);
+    // SetConsoleOutputCP(CP_UTF8); // Resolução de caracteres especiais no terminal do windows
+    // SetConsoleCP(CP_UTF8); // Resolução de caracteres especiais no terminal do windows
 
     printf("------------------------------------");
     printf("\n|           SUPER TRUNFO           |\n");
@@ -20,6 +20,8 @@ int main(void) {
     int qtd_pontos_turisticos_carta_1;
     float densidade_populacional_carta_1;
     float pib_per_capita_carta_1;
+    long double super_poder_carta_1;
+    float inverso_densidade_carta_1;
 
     // Dados da carta 2A
     char estado_carta_2;
@@ -31,6 +33,9 @@ int main(void) {
     int qtd_pontos_turisticos_carta_2;
     float densidade_populacional_carta_2;
     float pib_per_capita_carta_2;
+    long double super_poder_carta_2;
+    float inverso_densidade_carta_2;
+
 
     // Captura de dados da carta 1
     printf("Insira os dados da carta 1\n\n");
@@ -109,6 +114,28 @@ int main(void) {
     pib_per_capita_carta_2 =  pib_carta_2 / (float) populacao_carta_2;
     printf("Densidade Populacional: %.2f\n", densidade_populacional_carta_2);
     printf("PIB per capita: %.2f\n", pib_per_capita_carta_2);
+
+    printf("\n ------------------------------------ \n\n");
+
+    if (densidade_populacional_carta_1 > 0.0f) {
+        inverso_densidade_carta_1 = 1.0f / densidade_populacional_carta_1;
+    } else {
+        inverso_densidade_carta_1 = 0.0f;
+    }
+
+    super_poder_carta_1 = (float) populacao_carta_1 + area_km_carta_1 + pib_carta_1 + (float) qtd_pontos_turisticos_carta_1 + inverso_densidade_carta_1;
+
+    inverso_densidade_carta_2 = (densidade_populacional_carta_2 > 0.0f) ? 1.0f / densidade_populacional_carta_2 : 0.0f;
+    super_poder_carta_2 = (float) populacao_carta_2 + area_km_carta_2 + pib_carta_2 + (float) qtd_pontos_turisticos_carta_2 + inverso_densidade_carta_2;
+
+    printf("Comparação de Cartas:");
+    printf("População: %s\n", (populacao_carta_1 > populacao_carta_2) ? "Carta 1 Venceu" : (populacao_carta_2 > populacao_carta_1) ? "Carta 2 Venceu" : "Empate");
+    printf("Área: %s\n", (area_km_carta_1 > area_km_carta_2) ? "Carta 1 Venceu" : (area_km_carta_2 > area_km_carta_1) ? "Carta 2 Venceu" : "Empate");
+    printf("PIB: %s\n", (pib_carta_1 > pib_carta_2) ? "Carta 1 Venceu" : (pib_carta_2 > pib_carta_1) ? "Carta 2 Venceu" : "Empate");
+    printf("Pontos Turísticos: %s\n", (qtd_pontos_turisticos_carta_1 > qtd_pontos_turisticos_carta_2) ? "Carta 1 Venceu" : (qtd_pontos_turisticos_carta_2 > qtd_pontos_turisticos_carta_1) ? "Carta 2 Venceu" : "Empate");
+    printf("Densidade Populacional: %s\n", (densidade_populacional_carta_1 > densidade_populacional_carta_2) ? "Carta 1 Venceu" : (densidade_populacional_carta_2 > densidade_populacional_carta_1) ? "Carta 2 Venceu" : "Empate");
+    printf("PIB per Capita: %s\n", (pib_per_capita_carta_1 > pib_per_capita_carta_2) ? "Carta 1 Venceu" : (pib_per_capita_carta_2 > pib_per_capita_carta_1) ? "Carta 2 Venceu" : "Empate");
+    printf("Super Poder: %s\n", (super_poder_carta_1 > super_poder_carta_2) ? "Carta 1 Venceu" : (super_poder_carta_2 > super_poder_carta_1) ? "Carta 2 Venceu" : "Empate");
 
     return 0;
 }
